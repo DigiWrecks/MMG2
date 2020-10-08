4a08c361d31cc915739e86f362010537b0c958fb
dnadawa <40896729+dnadawa@users.noreply.github.com>
2020-10-08 09:10:30 +0530

    iOS Changes Added

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Afaq Ahmed on 14/09/2020.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
